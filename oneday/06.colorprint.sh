8f34684552e77f40f20b5cf427ad0b02a580527e
hlions <hlions@163.com>
2019-12-31 08:50:45 +0800

    oneday

---
+++ oneday/06.colorprint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+printf "\e[1;31m this is green \e[0m \n"
+printf "\e[1;44m \e[1;31m this is green \e[0m \e[0m \n"
---
+++ oneday/07.getresult.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+echo `date +%Y-%m-%d-%H:%M:%S`
+
+echo $(date +%Y-%m-%d-%H:%M:%S
